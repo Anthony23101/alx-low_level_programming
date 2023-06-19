37c424fb0c6c00c4a4cdaf6c0f07d888fc5fcc14
Anthony23101 <wipster2013@yahoo.com>
2023-06-18 23:48:56 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
 * main - main entry point
-* return: always 0(success)
-* code ends as expected
+* Return: must always be (0)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); /* code ends as expected */
+	return (0);
 }
